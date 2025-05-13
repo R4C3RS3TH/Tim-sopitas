845d8f1faa736d95f28f93c04a7570a28dc558e9
enzo <enzo.santillan@utec.edu.pe>
2025-05-13 17:18:08 -0500

    feat: se agrego lista de productos al dto

--- src/main/java/dbp/recuperation/hack1/order/dto/orderRequestDto.java
+++ src/main/java/dbp/recuperation/hack1/order/dto/orderRequestDto.java
@@ -1,8 +1,12 @@
 package dbp.recuperation.hack1.order.dto;
 
+import dbp.recuperation.hack1.productos.domain.Producto;
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 public class orderRequestDto {
     private String email;
+    private List<Producto> products;
 }
