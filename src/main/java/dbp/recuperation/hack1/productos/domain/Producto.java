06ed168727831f7827b557439d3136b52a750ab8
Susopita <thesusot@gmail.com>
2025-05-13 18:48:59 -0500

    feat: ggs

--- src/main/java/dbp/recuperation/hack1/productos/domain/Producto.java
+++ src/main/java/dbp/recuperation/hack1/productos/domain/Producto.java
@@ -1,14 +1,9 @@
 package dbp.recuperation.hack1.productos.domain;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-import dbp.recuperation.hack1.order.domain.Order;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
