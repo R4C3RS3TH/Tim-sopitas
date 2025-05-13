6887074be9ebb135bcabf470b9665d201040322a
enzo <enzo.santillan@utec.edu.pe>
2025-05-13 17:13:44 -0500

    fix: se arreglo el dto

--- src/main/java/dbp/recuperation/hack1/order/dto/orderRequestDto.java
+++ src/main/java/dbp/recuperation/hack1/order/dto/orderRequestDto.java
@@ -5,18 +5,5 @@ import lombok.Data;
 @Data
 public class orderRequestDto {
     private String email;
-    private String product;
-    private int quantity;
-    private String address;
-    private String paymentMethod;
-    private String orderStatus;
-
-    public orderRequestDto(String email, String product, int quantity, String address, String paymentMethod, String orderStatus) {
-        this.email = email;
-        this.product = product;
-        this.quantity = quantity;
-        this.address = address;
-        this.paymentMethod = paymentMethod;
-        this.orderStatus = orderStatus;
-    }
+    private List<Producto> product;
 }
