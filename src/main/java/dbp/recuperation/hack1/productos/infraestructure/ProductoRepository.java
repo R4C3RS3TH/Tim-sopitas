88f8d8e94e044d64b73d47157e4d708ae16d2b40
Susopita <thesusot@gmail.com>
2025-05-13 17:32:03 -0500

    s

--- src/main/java/dbp/recuperation/hack1/productos/infraestructure/ProductoRepository.java
+++ src/main/java/dbp/recuperation/hack1/productos/infraestructure/ProductoRepository.java
@@ -4,9 +4,6 @@ import dbp.recuperation.hack1.productos.domain.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
-public interface ProductoRepository extends JpaRepository<Producto, String> {
-    Optional<Producto> findById(String id);
+public interface ProductoRepository extends JpaRepository<Producto, Long> {
 }
